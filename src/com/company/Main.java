df7e803968dcd7736df29d0c58a7d702de1c3cbc
Todd <tk304@pvsd.org>
2018-09-10 19:53:30 -0400

    ☐ Very good
    ☑ Good
    
    ☐ Nothing special
    
    ☐ Ehh
    
    ☐ Bad
    
    ☐ Just dont

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,34 @@
 package com.company;
+import java.util.*;
 
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        Scanner scan = new Scanner(System.in);
+
+        String fName = null;
+        String lName = null;
+        System.out.println("Enter your first name");
+        try
+        {
+            fName = scan.nextLine();
+        }
+        catch (Exception ex)
+        {
+            System.out.println("Error: " + ex.getMessage());
+        }
+
+        System.out.println("Enter your last name");
+        try
+        {
+            lName = scan.nextLine();
+        }
+        catch (Exception ex)
+        {
+            System.out.println("Error: " + ex.getMessage());
+        }
+
+        System.out.println("Your full name is " + fName + " " + lName + ".");
     }
 }
